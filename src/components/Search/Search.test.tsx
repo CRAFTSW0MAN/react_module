bbb4ffccef8d74332801dd19be069241922ff1c2
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2023-11-13 18:20:40 +0600

    feat: add test search2

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -5,7 +5,7 @@ import { Search } from './Search';
 
 
 describe('Search', () => {
-  it('should save the entered value in local storage when the search button is clicked', () => {
+  it('Verify that clicking the Search button saves the entered value to the local storage', () => {
 
     const countPage=1;
     const countItemData=1;
@@ -34,9 +34,52 @@ describe('Search', () => {
     );
     const inputElement = screen.getByPlaceholderText('Search...');
     const searchButton = screen.getByAltText('SearchLogo');
+    const deleteButton = screen.getByAltText('DeleteLogo');
     fireEvent.change(inputElement, { target: { value: dataSearch } });
     fireEvent.click(searchButton);
+    fireEvent.click(deleteButton);
     const savedQuery = localStorage.getItem('searchQuery');
     expect(savedQuery).toBe('test query');
+    localStorage.removeItem('searchQuery');
   });
 });
+
+
+describe('Search component', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+test('Check that the component retrieves the value from the local storage upon mounting', () => {
+  const countPage=1;
+    const countItemData=1;
+    const dataSearch='';
+    const selectedValue=10;
+    const arrProducts:IdataProduct[]=[];
+    const handleUpdatePage= (page:number) => {console.log(page)};
+    const handleChangeSelect = (
+      event: React.ChangeEvent<HTMLSelectElement>
+    ) => {console.log(event)};
+    const handleUpdateSearch= (search:string) => {console.log(search)};
+
+    localStorage.setItem('searchQuery', 'test value');
+
+    render(
+      <MainContext.Provider
+      value={{
+        countPage,
+        countItemData,
+        dataSearch,
+        selectedValue,
+        arrProducts,
+        handleUpdatePage,
+        handleChangeSelect,
+        handleUpdateSearch,
+      }}>
+        <Search />
+      </MainContext.Provider>
+    );
+
+  expect(screen.getByPlaceholderText('Search...')).toHaveValue('test value');
+  localStorage.removeItem('searchQuery');
+});
+})
